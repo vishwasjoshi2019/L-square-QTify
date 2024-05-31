4312560b4c228a6b8eb2df1e5ff012f7b564a6fd
Vishwas Joshi <vishwas.j@iitgn.ac.in>
2024-05-31 14:40:58 +0530

    hero section completed

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 import styles from "./App.module.css"
+
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    <Hero />
     </div>
   );
 }
