63174574dd120713ee09826fd1741e6d9a98591b
Vishwas Joshi <vishwas.j@iitgn.ac.in>
2024-05-31 12:31:08 +0530

    used navbar component in app.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Hello i'm testing it   
-      </header>
+    <Navbar />
     </div>
   );
 }
